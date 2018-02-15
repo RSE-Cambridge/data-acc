9b3e4bfc661e4e8e65b502f7cd15b2681a11c600
John Garbutt <john@johngarbutt.com>
2018-02-15 14:51:02 +0000

    Ensure you push changes before pulling them

--- docker-slurm/update_burstbuffer.sh
+++ docker-slurm/update_burstbuffer.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 set -e
 
+git push
 docker exec slurmctld bash -c "cd /usr/local/src/burstbuffer && . .venv/bin/activate && git pull && pip install -Ue . && fakewarp help"
 docker-compose restart slurmctld
