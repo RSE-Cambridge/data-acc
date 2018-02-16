8e8483e954aee34c33e45d9dfeb5c0a068179484
John Garbutt <john@johngarbutt.com>
2018-02-16 12:36:16 +0000

    Add tests for execution facade

---
+++ burstbuffer/tests/test_execution_facade.py
@@ -0,0 +1,27 @@
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+import testtools
+
+from burstbuffer import execution_facade
+
+
+class TestExecutionFacade(testtools.TestCase):
+
+    def test_get_all_pool_stats(self):
+        all_pool_stats = execution_facade.get_all_pool_stats()
+
+        self.assertEqual(1, len(all_pool_stats))
+        self.assertEqual("dedicated_nvme", all_pool_stats[0].name)
+        self.assertEqual(20, all_pool_stats[0].total_slices)
+        self.assertEqual(10, all_pool_stats[0].free_slices)
+        self.assertEqual(10 ** 12, all_pool_stats[0].slice_bytes)
