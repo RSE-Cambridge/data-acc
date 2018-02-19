cb6b5322120b4a1bd89d7758465e9410f0c73aaf
John Garbutt <john@johngarbutt.com>
2018-02-19 16:04:46 +0000

    Add etcd proxy and add client into slurm image
    Lets look at using the native go client for now to get/set values in
    etcd. Basic proxy setup, for completeness. Added test of etcd into the
    update burstbuffer script.

--- docker-slurm/update_burstbuffer.sh
+++ docker-slurm/update_burstbuffer.sh
@@ -2,10 +2,15 @@
 set -e
 
 git push
-docker exec slurmctld bash -c "cd /usr/local/src/burstbuffer && . .venv/bin/activate && git remote update && git checkout add-facade && git pull && pip install -Ue . && fakewarp help"
+docker exec slurmctld bash -c "cd /usr/local/src/burstbuffer && . .venv/bin/activate && git remote update && git checkout etcd && git pull && pip install -Ue . && fakewarp help"
 docker-compose restart slurmctld
 
 sleep 2
+
+echo Test etcd
+docker exec slurmctld bash -c "ETCDCTL_API=3 etcdctl --endpoints=http://etcdproxy1:2379 get buffers -w json"
+ETCDCTL_API=3 etcdctl --endpoints=http://localhost:2379 get buffers -w json
+
 
 docker exec slurmctld bash -c "cd /data && su slurm -c 'srun --bb=\"capacity=1G\" bash -c \"set\"'"
 
