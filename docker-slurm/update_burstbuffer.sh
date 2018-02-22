0048e8ae6e077826375527beee88fe684d10b2dc
John Garbutt <john@johngarbutt.com>
2018-02-22 11:31:01 +0000

    Create persistent burst buffer demo

--- docker-slurm/update_burstbuffer.sh
+++ docker-slurm/update_burstbuffer.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-set -x
+set +x
 
 git push
 docker build -t slurm-docker-cluster:17.02.9 . --build-arg BURSTBUFFER_BRANCH=`git show HEAD -q | head -1 | cut -c 8-`
@@ -8,16 +8,44 @@ docker build -t slurm-docker-cluster:17.02.9 . --build-arg BURSTBUFFER_BRANCH=`g
 
 docker-compose up -d
 
-sleep 2
+docker exec slurmctld bash -c 'cd /data && echo "#!/bin/bash
+#BB create_persistent name=mytestbuffer capacity=32GB access=striped type=scratch" > create-persistent.sh'
+docker exec slurmctld bash -c 'cd /data && echo "#!/bin/bash
+#BB destroy_persistent name=mytestbuffer" > delete-persistent.sh'
 
-docker-compose logs -f &
+echo ***Show current system state***
+docker exec slurmctld bash -c "cd /data && scontrol show burstbuffer"
+
+sleep 3
+echo ***Create persistent buffer***
+docker exec slurmctld bash -c "cd /data && su slurm -c 'sbatch create-persistent.sh'"
 
 sleep 5
+docker exec slurmctld bash -c "cd /data && scontrol show burstbuffer"
 
-echo For more details run "export ETCDCTL_API=3 watch --prefix buffers/"
-docker exec slurmctld bash -c "cd /data && su slurm -c 'srun --bb=\"capacity=1G\" bash -c \"set\"'"
+echo ***Create per job buffer***
+docker exec slurmctld bash -c "cd /data && su slurm -c 'srun --bb=\"capacity=1G\" bash -c \"sleep 10 && echo \$HOSTNAME\"'" &
+sleep 5
+docker exec slurmctld bash -c "cd /data && scontrol show burstbuffer"
 
-sleep 15
+sleep 5
+echo ***Delete peristent buffer***
+docker exec slurmctld bash -c "cd /data && su slurm -c 'sbatch delete-persistent.sh'"
+
+sleep 22
+echo ***Show all is cleaned up***
+docker exec slurmctld bash -c "cd /data && scontrol show burstbuffer"
+
+#sleep 15
+
+# docker-compose logs -f &
+
+# sleep 5
+
+# echo For more details run "export ETCDCTL_API=3 watch --prefix buffers/"
+# docker exec slurmctld bash -c "cd /data && su slurm -c 'srun --bb=\"capacity=1G\" bash -c \"set\"'"
+
+# sleep 15
 
 #
 # Use this one to see logs as the job executes
@@ -26,4 +54,4 @@ sleep 15
 # docker exec slurmctld bash -c "cd /data && su slurm -c 'sbatch -n2 --bbf=buffer.txt --wrap=hostname'"
 # docker logs slurmctld -f
 
-kill %1
+# kill %1
