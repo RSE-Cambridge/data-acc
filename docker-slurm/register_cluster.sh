4097a18dae2f0ae29d184c99aca0e3d3c94ea4ad
John Garbutt <john@johngarbutt.com>
2018-03-15 17:32:30 +0000

    Update key in glusterfs

--- docker-slurm/register_cluster.sh
+++ docker-slurm/register_cluster.sh
@@ -47,3 +47,5 @@ docker exec gluster1 bash -c "mkdir -p /root/.ssh && echo $SSH_PUB_KEY > /root/.
 docker exec slurmctld bash -c "ssh -oStrictHostKeyChecking=no -p 2222 gluster1 hostname"
 docker exec gluster2 bash -c "mkdir -p /root/.ssh && echo $SSH_PUB_KEY > /root/.ssh/authorized_keys"
 docker exec slurmctld bash -c "ssh -oStrictHostKeyChecking=no -p 2222 gluster2 hostname"
+docker exec gluster3 bash -c "mkdir -p /root/.ssh && echo $SSH_PUB_KEY > /root/.ssh/authorized_keys"
+docker exec slurmctld bash -c "ssh -oStrictHostKeyChecking=no -p 2222 gluster3 hostname"
