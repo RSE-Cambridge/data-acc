f1462c8c68a8b1e26362d0ec36d7e06f7922f0f8
John Garbutt <john@johngarbutt.com>
2018-03-16 00:14:32 +0000

    Saw timeout, trying to increase the timeout

--- burstbuffer/provision/gluster.py
+++ burstbuffer/provision/gluster.py
@@ -23,14 +23,13 @@ def _exec_command(command, hostname, port=2222, username="root"):
         client.load_system_host_keys()
         client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
         print("connecting to: %s" % hostname)
-        client.connect(hostname, port, username, timeout=10)
+        client.connect(hostname, port, username, timeout=30)
 
         print("running command: %s" % command)
         stdin, stdout, stderr = client.exec_command(command, timeout=60.0)
 
         stdout_lines = stdout.readlines()
         stderr_lines = stderr.readline()
-
         #print(stdout_lines)
         #print(stderr_lines)
 
