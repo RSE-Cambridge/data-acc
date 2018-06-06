2c7dc8e2178a68c8a1fc89cfa7f7240a6bab0d1b
John Garbutt <john@johngarbutt.com>
2018-06-06 11:42:57 +0100

    Rename helpers to keystore

--- build/rebuild_mocks.sh
+++ build/rebuild_mocks.sh
@@ -1,6 +1,6 @@
 set -eux
 
 echo "Regenerate mocks:"                                                       
-mockgen -source=internal/pkg/keystoreregistry/helpers.go \
+mockgen -source=internal/pkg/keystoreregistry/keystore.go \
     -package keystoreregistry >internal/pkg/keystoreregistry/mocks.go
 
