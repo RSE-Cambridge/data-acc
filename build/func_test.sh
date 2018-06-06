85ba5529ffc9c0af0acbb38722db4423a3f5fb39
John Garbutt <john@johngarbutt.com>
2018-06-06 12:48:04 +0100

    Add script to run functional tests

---
+++ build/func_test.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+set -eu -o pipefail
+
+make
+
+export ETCDCTL_ENDPOINTS=127.0.0.1:2379
+export ETCDCTL_API=3
+
+echo
+echo run tests:
+./bin/amd64/etcd-keystore-func-test
+
+echo
+echo see side effects:
+etcdctl get --prefix ""
