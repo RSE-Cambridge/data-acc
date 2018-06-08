18f5ce691ed9a938099e50d06692e9c31ecc1162
John Garbutt <john@johngarbutt.com>
2018-06-08 21:34:48 +0100

    Make tests compile

--- internal/pkg/fakewarp/pools_test.go
+++ internal/pkg/fakewarp/pools_test.go
@@ -3,10 +3,16 @@ package fakewarp
 import (
 	"github.com/stretchr/testify/assert"
 	"testing"
+	"github.com/golang/mock/gomock"
+	"github.com/RSE-Cambridge/data-acc/internal/pkg/keystoreregistry"
 )
 
 func TestGetPools(t *testing.T) {
-	actual := GetPools().String()
+	mockCtrl := gomock.NewController(t)
+	mockObj := keystoreregistry.NewMockKeystore(mockCtrl)
+
+	pools, _ := GetPools(keystoreregistry.NewPoolRegistry(mockObj))
+	actual := pools.String()
 	expected := `{"pools":[{"id":"fake","units":"bytes","granularity":214748364800,"quantity":400,"free":395}]}`
 	assert.EqualValues(t, expected, actual[:len(actual)-1])
 	assert.EqualValues(t, "\n", actual[len(actual)-1:])
