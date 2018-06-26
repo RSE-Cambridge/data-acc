962f36edbba1f428b48dcce09562a733e76d2422
John Garbutt <john@johngarbutt.com>
2018-06-26 09:18:24 +0100

    Add better example job
    Includes using persistent buffer, with per job buffer that is striped
    and private with swap and data_in and data_out specified.

--- docker-slurm/update_burstbuffer.sh
+++ docker-slurm/update_burstbuffer.sh
@@ -26,6 +26,15 @@ docker exec slurmctld bash -c 'cd /data && echo "#!/bin/bash
 #BB create_persistent name=mytestbuffer capacity=4000GB access=striped type=scratch" > create-persistent.sh'
 docker exec slurmctld bash -c 'cd /data && echo "#!/bin/bash
 #BB destroy_persistent name=mytestbuffer" > delete-persistent.sh'
+docker exec slurmctld bash -c 'cd /data && echo "#!/bin/bash
+#DW jobdw capacity=2TB access_mode=striped,private type=scratch
+#DW persistentdw name=mytestbuffer
+#DW swap 1GB
+#DW stage_in source=/global/cscratch1/filename1 destination=$DW_JOB_STRIPED/filename1 type=file
+#DW stage_out source=$DW_JOB_STRIPED/outdir destination=/global/scratch1/outdir type=directory
+set
+echo \$HOSTNAME
+" > use-persistent.sh'
 
 echo "***Show current system state***"
 docker exec slurmctld bash -c "cd /data && scontrol show burstbuffer"
@@ -45,6 +54,13 @@ sleep 5
 docker exec slurmctld bash -c "cd /data && scontrol show burstbuffer"
 
 sleep 5
+echo "***Use persistent buffer***"
+sleep 5
+docker exec slurmctld bash -c "cd /data && cat use-persistent.sh"
+docker exec slurmctld bash -c "cd /data && su slurm -c 'sbatch use-persistent.sh'"
+sleep 5
+docker exec slurmctld bash -c "cd /data && scontrol show burstbuffer"
+
 echo "***Delete persistent buffer***"
 docker exec slurmctld bash -c "cd /data && cat delete-persistent.sh"
 docker exec slurmctld bash -c "cd /data && su slurm -c 'sbatch delete-persistent.sh'"
