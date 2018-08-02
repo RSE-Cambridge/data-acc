9630ffdfe7ca3c32396d8aa80decd92bf204e4c5
John Garbutt <john@johngarbutt.com>
2018-08-02 10:37:16 +0100

    Make demo script better for when mounting real PFS

--- ansible/demo.sh
+++ ansible/demo.sh
@@ -2,17 +2,17 @@
 set +eux
 
 docker exec slurmctld bash -c 'cd /data && echo "#!/bin/bash
-#BB create_persistent name=mytestbuffer capacity=2000GB access=striped type=scratch" > create-persistent.sh'
+#BB create_persistent name=mydac capacity=2000GB access=striped type=scratch" > create-persistent.sh'
 docker exec slurmctld bash -c 'cd /data && echo "#!/bin/bash
-#BB destroy_persistent name=mytestbuffer" > delete-persistent.sh'
+#BB destroy_persistent name=mydac" > delete-persistent.sh'
 docker exec slurmctld bash -c 'cd /data && echo "#!/bin/bash
 #DW jobdw capacity=2TB access_mode=striped,private type=scratch
-#DW persistentdw name=mytestbuffer
+#DW persistentdw name=mydac
 #DW swap 1GB
 #DW stage_in source=/global/cscratch1/filename1 destination=\$DW_JOB_STRIPED/filename1 type=file
 #DW stage_out source=\$DW_JOB_STRIPED/outdir destination=/global/scratch1/outdir type=directory
 set
-echo \$HOSTNAME
+hostname
 " > use-persistent.sh'
 
 echo "***Show current system state***"
@@ -25,26 +25,39 @@ docker exec slurmctld bash -c "cd /data && su slurm -c 'sbatch create-persistent
 
 sleep 5
 docker exec slurmctld bash -c "cd /data && scontrol show burstbuffer"
+docker exec slurmctld bash -c "cd /data && squeue"
+echo
 
 echo "***Create per job buffer***"
-echo 'srun --bb="capacity=1TB" bash -c "sleep 10 && echo \$HOSTNAME"'
-docker exec slurmctld bash -c "cd /data && su slurm -c 'srun --bb=\"capacity=100GB\" bash -c \"sleep 5 && echo \$HOSTNAME\"'" &
+echo 'srun --bb="capacity=1TB" bash -c "sleep 10 && hostname"'
+docker exec slurmctld bash -c "cd /data && su slurm -c 'srun --bb=\"capacity=100GB\" bash -c \"sleep 5 && hostname\"'"
 sleep 5
 docker exec slurmctld bash -c "cd /data && scontrol show burstbuffer"
 
 sleep 5
+echo
 echo "***Use persistent buffer***"
-sleep 5
 docker exec slurmctld bash -c "cd /data && cat use-persistent.sh"
 docker exec slurmctld bash -c "cd /data && su slurm -c 'sbatch use-persistent.sh'"
-sleep 5
+sleep 10
+docker exec slurmctld bash -c "cd /data && squeue"
+docker exec slurmctld bash -c "cd /data && scontrol show burstbuffer"
+sleep 30
+docker exec slurmctld bash -c "cd /data && squeue"
 docker exec slurmctld bash -c "cd /data && scontrol show burstbuffer"
 
 echo "***Delete persistent buffer***"
 docker exec slurmctld bash -c "cd /data && cat delete-persistent.sh"
 docker exec slurmctld bash -c "cd /data && su slurm -c 'sbatch delete-persistent.sh'"
 
-sleep 22
+sleep 30
+docker exec slurmctld bash -c "cd /data && scontrol show burstbuffer"
+docker exec slurmctld bash -c "cd /data && squeue"
+sleep 30
+docker exec slurmctld bash -c "cd /data && scontrol show burstbuffer"
+docker exec slurmctld bash -c "cd /data && squeue"
+sleep 30
+echo
 echo "***Show all buffers are cleaned up***"
 docker exec slurmctld bash -c "cd /data && scontrol show burstbuffer"
 echo "***Show all jobs completed***"
@@ -60,4 +73,3 @@ docker logs slurm-master_brickwatcher_1
 sleep 3
 echo "***Data still in etcd ***"
 docker exec slurm-master_brickwatcher_1 bash -c "etcdctl get --prefix ''"
-
