a864f2fdf809628ac58ac7d908c7fc38b0d7b418
John Garbutt <john@johngarbutt.com>
2018-08-03 13:57:33 +0100

    Check fs2 is working in demo script

--- fs-ansible/environment/demo.sh
+++ fs-ansible/environment/demo.sh
@@ -15,6 +15,7 @@ ansible-playbook test-dac.yml -i test-inventory --tag start_mgs --tag start_mdts
 echo Show FS works
 echo
 
+ls /mnt/lustre/fs1/demo
 ssh dac2 'sudo bash -c "hostname > /mnt/lustre/fs1/demo"'
 cat /mnt/lustre/fs1/demo
 
@@ -37,9 +38,16 @@ echo Bring up an down FS2
 ansible-playbook test-dac2.yml -i test-inventory2 --tag format_mgs --tag reformat_mdts --tag reformat_osts
 ansible-playbook test-dac2.yml -i test-inventory2 --tag start_mgs --tag start_mdts --tag start_osts --tag mount_fs
 ansible-playbook test-dac2.yml -i test-inventory2 --tag start_mgs --tag start_mdts --tag start_osts --tag mount_fs
+
+ls /mnt/lustre/fs2/demo
+ssh dac2 'sudo bash -c "hostname > /mnt/lustre/fs2/demo"'
+cat /mnt/lustre/fs2/demo
+
 ansible-playbook test-dac2.yml -i test-inventory2 --tag umount_fs --tag stop_osts --tag stop_mdts
 ansible-playbook test-dac2.yml -i test-inventory2 --tag umount_fs --tag stop_osts --tag stop_mdts
 ansible-playbook test-dac2.yml -i test-inventory2 --tag reformat_mdts --tag reformat_osts
+
+cat /mnt/lustre/fs2/demo || true
 
 echo Clean MGS
 echo
