d7fdd50072185e3f2b978b50e309b5ed28d4a543
John Garbutt <john@johngarbutt.com>
2018-08-07 13:14:20 +0100

    First attempt at a real mount and umount

--- internal/pkg/pfsprovider/fake/mount.go
+++ internal/pkg/pfsprovider/fake/mount.go
@@ -4,10 +4,12 @@ import (
 	"fmt"
 	"github.com/RSE-Cambridge/data-acc/internal/pkg/registry"
 	"log"
+	"os/exec"
 	"path"
 )
 
 func getMountDir(volume registry.Volume) string {
+	// TODO: what about the environment variables that are being set? should share logic with here
 	if volume.MultiJob {
 		return fmt.Sprintf("/mnt/multi_job_buffer/%s", volume.UUID)
 	}
@@ -15,8 +17,6 @@ func getMountDir(volume registry.Volume) string {
 }
 
 func mount(volume registry.Volume, brickAllocations []registry.BrickAllocation) error {
-	log.Println("FAKE Mount for:", volume.Name)
-
 	var primaryBrickHost string
 	for _, allocation := range brickAllocations {
 		if allocation.AllocatedIndex == 0 {
@@ -31,9 +31,13 @@ func mount(volume registry.Volume, brickAllocations []registry.BrickAllocation) 
 
 	var mountDir = getMountDir(volume)
 	for _, attachment := range volume.Attachments {
-		log.Printf("FAKE ssh %s mount -t lustre %s:/%s %s",
-			attachment.Hostname, primaryBrickHost, volume.UUID, mountDir)
-		// TODO: what about the environment variables that are being set? should share logic with here
+
+		if err := mkdir(attachment.Hostname, mountDir); err != nil {
+			return err
+		}
+		if err := mountLustre(attachment.Hostname, primaryBrickHost, volume.UUID, mountDir); err != nil {
+			return err
+		}
 
 		if !volume.MultiJob && volume.AttachAsSwapBytes > 0 {
 			swapDir := path.Join(mountDir, fmt.Sprintf("/swap/%s", attachment.Hostname))
@@ -63,7 +67,46 @@ func umount(volume registry.Volume, brickAllocations []registry.BrickAllocation)
 			swapDir := path.Join(mountDir, fmt.Sprintf("/swap/%s", attachment.Hostname))
 			log.Printf("FAKE swapoff %s", swapDir)
 		}
-		log.Printf("FAKE ssh %s umount %s", attachment.Hostname, mountDir)
+		if err := umountLustre(attachment.Hostname, mountDir); err != nil {
+			return err
+		}
+		if err := removeSubtree(attachment.Hostname, mountDir); err != nil {
+			return err
+		}
 	}
 	return nil
+}
+
+func umountLustre(hostname string, directory string) error {
+	return remoteExecuteCmd(hostname, fmt.Sprintf("umount -l %s", directory))
+}
+
+func removeSubtree(hostname string, directory string) error {
+	return remoteExecuteCmd(hostname, fmt.Sprintf("rm -rf %s", directory))
+}
+
+func mountLustre(hostname string, mgtHost string, fsname string, directory string) error {
+	return remoteExecuteCmd(hostname, fmt.Sprintf(
+		"mount -t lustre %s:/%s %s", mgtHost, fsname, directory))
+}
+
+func mkdir(hostname string, directory string) error {
+	return remoteExecuteCmd(hostname, fmt.Sprintf("mkdir -p %s", directory))
+}
+
+func remoteExecuteCmd(hostname string, cmdStr string) error {
+	log.Println("SSH to:", hostname, "with command:", cmdStr)
+
+	cmd := exec.Command("ssh", "-o", "StrictHostKeyChecking=no",
+		"-o", "UserKnownHostsFile=/dev/null", hostname, "sudo", cmdStr)
+	output, err := cmd.CombinedOutput()
+
+	if err == nil {
+		log.Println("Completed remote ssh run:", cmdStr)
+		log.Println(string(output))
+		return nil
+	} else {
+		log.Println("Error in remove ssh run:", string(output))
+		return err
+	}
 }
