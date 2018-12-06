0d30dcf616304597cfa588f2d52a9ff177be954e
John Garbutt <john@johngarbutt.com>
2018-12-06 15:22:37 +0000

    Add functional test run
    Currently it doesn't pass, but at least it is running in this branch.

--- build/func_test.sh
+++ build/func_test.sh
@@ -2,30 +2,24 @@
 
 set -eu -o pipefail
 
-make
-#make test
+DOWNLOAD_URL=https://github.com/etcd-io/etcd/releases/download
+ETCD_VER=v3.3.10
+ETCDCTL_API=3
 
-export ETCDCTL_ENDPOINTS=127.0.0.1:2379
-export ETCDCTL_API=3
-
-echo
-echo run tests:
-## if etcd is not running in docker or localy run etcd &
-
-if [ ! -f ./bin/amd64/dacd ]; then
-  $GOPATH/bin/dacd &
-  $GOPATH/bin/etcd-keystore-func-test
-  kill %1
-
-  echo
-  echo see side effects:
-  etcdctl get --prefix ""
-else
-  ./bin/amd64/dacd &
-  ./bin/amd64/etcd-keystore-func-test
-  kill %1
-
-  echo
-  echo see side effects:
-  etcdctl get --prefix ""
+if [ ! -d /tmp/etcd-download ]; then
+    curl -L ${DOWNLOAD_URL}/${ETCD_VER}/etcd-${ETCD_VER}-linux-amd64.tar.gz -o /tmp/etcd-${ETCD_VER}-linux-amd64.tar.gz
+    rm -rf /tmp/etcd-download
+    mkdir /tmp/etcd-download
+    tar xzvf /tmp/etcd-${ETCD_VER}-linux-amd64.tar.gz -C /tmp/etcd-download --strip-components=1
+    rm -f /tmp/etcd-${ETCD_VER}-linux-amd64.tar.gz
 fi
+/tmp/etcd-download/etcd --version
+ETCDCTL_API=3 /tmp/etcd-download/etcdctl version
+
+make
+
+# be sure to kill etcd even if we hit an error
+trap 'kill $(jobs -p)' EXIT
+/tmp/etcd-download/etcd &
+
+ETCDCTL_ENDPOINTS=127.0.0.1:2379 ./bin/dac-func-test
