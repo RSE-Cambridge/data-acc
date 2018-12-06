5ff043e67384cbf19633a9d14804bf7f92028535
John Garbutt <john@johngarbutt.com>
2018-12-06 15:28:44 +0000

    Generate new style inventory
    Add index to mdt sent to fs-ansible
    Give every ost an mdt to match new lustre ansible

--- internal/pkg/pfsprovider/ansible/ansible_test.go
+++ internal/pkg/pfsprovider/ansible/ansible_test.go
@@ -30,9 +30,10 @@ func TestPlugin_GetInventory(t *testing.T) {
         cpu2: {}
         dac1:
           abcdefgh_mgs: nvme1n1
-          abcdefgh_mdt: nvme1n1
-          abcdefgh_osts: {nvme2n1: 1, nvme3n1: 2}
+          abcdefgh_mdts: {nvme1n1: 0, nvme2n1: 1, nvme3n1: 2}
+          abcdefgh_osts: {nvme1n1: 0, nvme2n1: 1, nvme3n1: 2}
         dac2:
+          abcdefgh_mdts: {nvme2n1: 3, nvme3n1: 4}
           abcdefgh_osts: {nvme2n1: 3, nvme3n1: 4}
       vars:
         abcdefgh_client_port: "10002"
@@ -55,8 +56,10 @@ func TestPlugin_GetInventory_withNoOstOnOneHost(t *testing.T) {
       hosts:
         dac1:
           abcdefgh_mgs: sdb
-          abcdefgh_mdt: nvme1n1
+          abcdefgh_mdts: {nvme1n1: 0}
+          abcdefgh_osts: {nvme1n1: 0}
         dac2:
+          abcdefgh_mdts: {nvme2n1: 1}
           abcdefgh_osts: {nvme2n1: 1}
       vars:
         abcdefgh_client_port: "10002"
