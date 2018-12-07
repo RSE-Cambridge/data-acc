ec58592ca75296dbab4ad830423933c83e9ceccf
John Garbutt <john@johngarbutt.com>
2018-12-07 00:12:12 +0000

    Add test for mounting lustre

--- internal/pkg/pfsprovider/ansible/mount_test.go
+++ internal/pkg/pfsprovider/ansible/mount_test.go
@@ -27,6 +27,7 @@ func Test_mkdir(t *testing.T) {
 	defer func() {runner = &run{}}()
 	fake := &fakeRunner{}
 	runner = fake
+
 	err := mkdir("host", "dir")
 	assert.Nil(t, err)
 	assert.Equal(t, "host", fake.hostnames[0])
@@ -34,5 +35,22 @@ func Test_mkdir(t *testing.T) {
 
 	runner = &fakeRunner{err: errors.New("expected")}
 	err = mkdir("", "")
+	assert.Equal(t, "expected", err.Error())
+}
+
+func Test_mountLustre(t *testing.T) {
+	defer func() {runner = &run{}}()
+	fake := &fakeRunner{}
+	runner = fake
+
+	err := mountLustre("host", "mgt", "fs", "dir")
+	assert.Nil(t, err)
+	assert.Equal(t, "host", fake.hostnames[0])
+	assert.Equal(t, "host", fake.hostnames[1])
+	assert.Equal(t, "modprobe -v lustre", fake.cmdStrs[0])
+	assert.Equal(t, "mount -t lustre mgt:/fs dir", fake.cmdStrs[1])
+
+	runner = &fakeRunner{err: errors.New("expected")}
+	err = mountLustre("", "", "", "")
 	assert.Equal(t, "expected", err.Error())
 }
