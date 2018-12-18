2b3c5c6cc679d61fc3061905c551aed3c6087930
John Garbutt <john@johngarbutt.com>
2018-12-18 17:49:39 +0000

    Ideas around rsync

--- internal/pkg/pfsprovider/ansible/copy.go
+++ internal/pkg/pfsprovider/ansible/copy.go
@@ -11,6 +11,10 @@ func processDataCopy(volume registry.Volume, request registry.DataCopyRequest) e
 	if err != nil {
 		return err
 	}
+	if cmd == "" {
+		log.Println("No files to copy for:", volume.Name)
+		return nil
+	}
 
 	log.Printf("FAKE copy: %s", cmd)
 	return nil
@@ -22,24 +26,24 @@ func generateDataCopyCmd(volume registry.Volume, request registry.DataCopyReques
 		return "", err
 	}
 
-	cmd := fmt.Sprintf("sudo su `getent passwd %d | cut -d: -f1` %s", volume.Owner, rsync)
+	cmd := fmt.Sprintf("sudo -g '#%d' -u '#%d' %s", volume.Group, volume.Owner, rsync)
+	cmd = fmt.Sprintf("bash -c \"export JOB='%s' && %s\"", volume.JobName, cmd)
 	return cmd, nil
 }
 
 func generateRsyncCmd(volume registry.Volume, request registry.DataCopyRequest) (string, error) {
 	if request.Source == "" && request.Destination == "" {
-		log.Println("No files to copy for:", volume.Name)
 		return "", nil
 	}
 
 	var flags string
 	if request.SourceType == registry.Directory {
-		flags = "-r "
+		flags = "-r -ospgu --stats"
 	} else if request.SourceType == registry.File {
-		flags = ""
+		flags = "-ospgu --stats"
 	} else {
 		return "", fmt.Errorf("unsupported source type %s for volume: %s", request.SourceType, volume.Name)
 	}
 
-	return fmt.Sprintf("rsync %s%s %s", flags, request.Source, request.Destination), nil
+	return fmt.Sprintf("rsync %s %s %s", flags, request.Source, request.Destination), nil
 }
