e4e45568e50c27e6e30bfaa263e2d1001094dc13
John Garbutt <john@johngarbutt.com>
2018-12-18 10:16:50 +0000

    Add tests for mount

--- internal/pkg/pfsprovider/ansible/mount_test.go
+++ internal/pkg/pfsprovider/ansible/mount_test.go
@@ -2,6 +2,7 @@ package ansible
 
 import (
 	"errors"
+	"github.com/RSE-Cambridge/data-acc/internal/pkg/registry"
 	"github.com/stretchr/testify/assert"
 	"testing"
 )
@@ -78,4 +79,60 @@ func Test_chown(t *testing.T) {
 	assert.Equal(t, "expected", err.Error())
 	assert.Equal(t, "host", fake.hostnames[0])
 	assert.Equal(t, "chown 10:11 dir", fake.cmdStrs[0])
+}
+
+func Test_Mount(t *testing.T) {
+	defer func() { runner = &run{} }()
+	fake := &fakeRunner{}
+	runner = fake
+	volume := registry.Volume{
+		Name: "asdf", JobName: "asdf",
+		AttachGlobalNamespace:  true,
+		AttachPrivateNamespace: true,
+		AttachAsSwapBytes:      10000,
+		Attachments: []registry.Attachment{
+			{Hostname: "client1", Job: "job1", State: registry.RequestAttach},
+			{Hostname: "client2", Job: "job1", State: registry.Attached},
+			{Hostname: "client2", Job: "job2", State: registry.RequestDetach},
+		},
+		ClientPort: 42,
+		Owner:      1001,
+		Group:      1001,
+	}
+
+	assert.PanicsWithValue(t,
+		"failed to find primary brick for volume: asdf",
+		func() { mount(Lustre, volume, nil) })
+
+	bricks := []registry.BrickAllocation{
+		{Hostname: "host1"},
+		{Hostname: "host2"},
+	}
+	err := mount(Lustre, volume, bricks)
+	assert.Nil(t, err)
+	assert.Equal(t, 39, fake.calls)
+
+	assert.Equal(t, "client1", fake.hostnames[0])
+	assert.Equal(t, "mkdir -p /dac/job1/job", fake.cmdStrs[0])
+	assert.Equal(t, "modprobe -v lustre", fake.cmdStrs[1])
+	assert.Equal(t, "mount -t lustre host1:/ /dac/job1/job", fake.cmdStrs[2])
+
+	assert.Equal(t, "mkdir -p /dac/job1/job/swap", fake.cmdStrs[3])
+	assert.Equal(t, "dd if=/dev/zero of=/dac/job1/job/swap/client1 bs=1024 count=2048 && sudo chmod 0600 /dac/job1/job/swap/client1", fake.cmdStrs[4])
+	assert.Equal(t, "losetup /dev/loop42 /dac/job1/job/swap/client1", fake.cmdStrs[5])
+	assert.Equal(t, "mkswap /dev/loop42", fake.cmdStrs[6])
+	assert.Equal(t, "swapon /dev/loop42", fake.cmdStrs[7])
+	assert.Equal(t, "mkdir -p /dac/job1/job/private/client1", fake.cmdStrs[8])
+	assert.Equal(t, "chown 1001:1001 /dac/job1/job/private/client1", fake.cmdStrs[9])
+	assert.Equal(t, "ln -s /dac/job1/job/private/client1 /dac/asdf/job_private", fake.cmdStrs[10])
+
+	assert.Equal(t, "mkdir -p /dac/job1/job/global", fake.cmdStrs[11])
+	assert.Equal(t, "chown 1001:1001 /dac/job1/job/global", fake.cmdStrs[12])
+
+	assert.Equal(t, "client2", fake.hostnames[13])
+	assert.Equal(t, "mkdir -p /dac/job1/job", fake.cmdStrs[13])
+
+	assert.Equal(t, "client2", fake.hostnames[26])
+	assert.Equal(t, "mkdir -p /dac/job2/job", fake.cmdStrs[26])
+
 }
