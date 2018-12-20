71134c291a914dd9fd30c0c6aaf972e2b9e3f56a
John Garbutt <john@johngarbutt.com>
2018-12-20 12:29:30 +0000

    Add restricted sudoers rules

--- tools/slurm-test.sh
+++ tools/slurm-test.sh
@@ -11,7 +11,7 @@ echo "#!/bin/bash
 echo "#!/bin/bash
 #DW jobdw capacity=2TB access_mode=striped,private type=scratch
 #DW persistentdw name=mytestbuffer
-#DW swap 1GB
+#DW swap 5MB
 #DW stage_in source=/global/cscratch1/filename1 destination=\$DW_JOB_STRIPED/filename1 type=file
 #DW stage_out source=\$DW_JOB_STRIPED/outdir destination=/global/scratch1/outdir type=directory
 set
