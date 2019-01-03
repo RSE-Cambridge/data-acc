8afdee9a55cca5045d24a133910811abd0e48ba3
John Garbutt <john@johngarbutt.com>
2019-01-03 11:25:14 +0000

    Check all watches processed

--- internal/pkg/etcdregistry/watch_test.go
+++ internal/pkg/etcdregistry/watch_test.go
@@ -3,6 +3,7 @@ package etcdregistry
 import (
 	"context"
 	"github.com/coreos/etcd/clientv3"
+	"github.com/coreos/etcd/mvcc/mvccpb"
 	"github.com/stretchr/testify/assert"
 	"testing"
 )
@@ -14,7 +15,7 @@ type fakeWatcher struct {
 }
 
 func (fw fakeWatcher) Watch(ctx context.Context, key string, opts ...clientv3.OpOption) clientv3.WatchChan {
-	assert.Equal(fw.t, "key1", key)
+	assert.Equal(fw.t, "key", key)
 	assert.EqualValues(fw.t, len(fw.opts), len(opts)) // TODO: how to assert this properly?
 	return fw.ch
 }
@@ -26,11 +27,71 @@ func TestEtcKeystore_Watch_Nil(t *testing.T) {
 	keystore := etcKeystore{
 		Watcher: fakeWatcher{
 			t: t, ch: nil,
+			opts: []clientv3.OpOption{clientv3.WithPrevKV()},
+		},
+	}
+
+	response := keystore.Watch(context.TODO(), "key", false)
+
+	assert.Empty(t, response)
+}
+
+func TestEtcKeystore_Watch(t *testing.T) {
+	ch := make(chan clientv3.WatchResponse)
+
+	keystore := etcKeystore{
+		Watcher: fakeWatcher{
+			t: t, ch: ch,
 			opts: []clientv3.OpOption{clientv3.WithPrefix(), clientv3.WithPrevKV()},
 		},
 	}
 
-	response := keystore.Watch(context.TODO(), "key1", true)
+	go func(){
+		ch <- clientv3.WatchResponse{
+			Events: []*clientv3.Event{
+				{Type: clientv3.EventTypePut, Kv: &mvccpb.KeyValue{Key:[]byte("key1")}},
+				{Type: clientv3.EventTypePut, Kv: &mvccpb.KeyValue{Key:[]byte("key2")}},
+		}}
+		ch <- clientv3.WatchResponse{
+			Events: []*clientv3.Event{
+				{Type: clientv3.EventTypePut, Kv: &mvccpb.KeyValue{CreateRevision:1}},
+			}}
+		ch <- clientv3.WatchResponse{
+			Events: []*clientv3.Event{
+				{Type: clientv3.EventTypeDelete, Kv: &mvccpb.KeyValue{}},
+				{Type: clientv3.EventTypeDelete, Kv: &mvccpb.KeyValue{}},
+			}}
+		close(ch)
+	}()
 
-	assert.Empty(t, response)
+	response := keystore.Watch(context.TODO(), "key", true)
+
+	ev1 := <- response
+	assert.True(t, ev1.IsCreate)
+	assert.False(t, ev1.IsModify)
+	assert.False(t, ev1.IsDelete)
+
+	ev2 := <- response
+	assert.True(t, ev2.IsCreate)
+	assert.False(t, ev2.IsModify)
+	assert.False(t, ev2.IsDelete)
+
+	ev3 := <- response
+	assert.False(t, ev3.IsCreate)
+	assert.True(t, ev3.IsModify)
+	assert.False(t, ev3.IsDelete)
+
+	ev4 := <- response
+	assert.False(t, ev4.IsCreate)
+	assert.False(t, ev4.IsModify)
+	assert.True(t, ev4.IsDelete)
+
+	ev5 := <- response
+	assert.False(t, ev5.IsCreate)
+	assert.False(t, ev5.IsModify)
+	assert.True(t, ev5.IsDelete)
+
+	// Check chan is closed
+	_, ok := <- response
+	assert.False(t, ok)
 }
