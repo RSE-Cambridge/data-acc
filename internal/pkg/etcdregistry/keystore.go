e0d2ac95c14992e6db8e23eaa3af1775839aee74
John Garbutt <john@johngarbutt.com>
2019-01-03 10:13:49 +0000

    Extract watch method

--- internal/pkg/etcdregistry/keystore.go
+++ internal/pkg/etcdregistry/keystore.go
@@ -236,51 +236,6 @@ func (client *etcKeystore) WatchKey(ctxt context.Context, key string,
 	}()
 }
 
-func (client *etcKeystore) Watch(ctxt context.Context, key string, withPrefix bool) keystoreregistry.KeyValueUpdateChan {
-	options := []clientv3.OpOption{clientv3.WithPrevKV()}
-	if withPrefix {
-		options = append(options, clientv3.WithPrefix())
-	}
-	rch := client.Client.Watch(ctxt, key, options...)
-
-	c := make(chan keystoreregistry.KeyValueUpdate)
-
-	go processWatchEvents(rch, c)
-
-	return c
-}
-
-func processWatchEvents(watchChan clientv3.WatchChan, c chan keystoreregistry.KeyValueUpdate) {
-	for watchResponse := range watchChan {
-		// if error, send empty update with an error
-		err := watchResponse.Err()
-		if err != nil {
-			c <- keystoreregistry.KeyValueUpdate{Err: err}
-		}
-
-		// send all events in this watch response
-		for _, ev := range watchResponse.Events {
-			update := keystoreregistry.KeyValueUpdate{
-				IsCreate: ev.IsCreate(),
-				IsModify: ev.IsModify(),
-				IsDelete: ev.Type == clientv3.EventTypeDelete,
-			}
-			if update.IsCreate || update.IsModify {
-				update.New = getKeyValueVersion(ev.Kv)
-			}
-			if update.IsDelete || update.IsModify {
-				update.Old = getKeyValueVersion(ev.PrevKv)
-			}
-
-			c <- update
-		}
-	}
-
-	// Assuming we get here when the context is cancelled or hits its timeout
-	// i.e. there are no more events, so we close the channel
-	close(c)
-}
-
 // TODO: this needs fixing up
 func (client *etcKeystore) WatchForCondition(ctxt context.Context, key string, fromRevision int64,
 	check func(update keystoreregistry.KeyValueUpdate) bool) (bool, error) {
