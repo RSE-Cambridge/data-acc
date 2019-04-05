2a8502365c0f518294cc65ddefd03ed28773658d
John <john.garbutt@stackhpc.com>
2019-04-05 14:38:21 +0100

    Re-work unit test to see mulitple mtds per server

--- internal/pkg/pfsprovider/ansible/ansible_test.go
+++ internal/pkg/pfsprovider/ansible/ansible_test.go
@@ -110,11 +110,16 @@ func TestPlugin_GetInventory_MaxMDT(t *testing.T) {
 	}
 
 	var brickAllocations []registry.BrickAllocation
-	for i :=1; i <= 26; i++ {
+	for i :=1; i <= 26; i = i+2 {
 		brickAllocations = append(brickAllocations, registry.BrickAllocation{
 			Hostname:       fmt.Sprintf("dac%d", i),
 			Device:         "nvme1n1",
 			AllocatedIndex: uint(i - 1),
+		})
+		brickAllocations = append(brickAllocations, registry.BrickAllocation{
+			Hostname:       fmt.Sprintf("dac%d", i),
+			Device:         "nvme2n1",
+			AllocatedIndex: uint(i),
 		})
 	}
 
@@ -127,81 +132,43 @@ func TestPlugin_GetInventory_MaxMDT(t *testing.T) {
         cpu2: {}
         dac1:
           abcdefgh_mgs: nvme1n1
-          abcdefgh_mdts: {nvme1n1: 0}
-          abcdefgh_osts: {nvme1n1: 0}
-        dac2:
-          abcdefgh_mdts: {nvme1n1: 1}
-          abcdefgh_osts: {nvme1n1: 1}
+          abcdefgh_mdts: {nvme1n1: 0, nvme2n1: 1}
+          abcdefgh_osts: {nvme1n1: 0, nvme2n1: 1}
         dac3:
-          abcdefgh_mdts: {nvme1n1: 2}
-          abcdefgh_osts: {nvme1n1: 2}
-        dac4:
-          abcdefgh_mdts: {nvme1n1: 3}
-          abcdefgh_osts: {nvme1n1: 3}
+          abcdefgh_mdts: {nvme1n1: 2, nvme2n1: 3}
+          abcdefgh_osts: {nvme1n1: 2, nvme2n1: 3}
         dac5:
-          abcdefgh_mdts: {nvme1n1: 4}
-          abcdefgh_osts: {nvme1n1: 4}
-        dac6:
-          abcdefgh_mdts: {nvme1n1: 5}
-          abcdefgh_osts: {nvme1n1: 5}
+          abcdefgh_mdts: {nvme1n1: 4, nvme2n1: 5}
+          abcdefgh_osts: {nvme1n1: 4, nvme2n1: 5}
         dac7:
-          abcdefgh_mdts: {nvme1n1: 6}
-          abcdefgh_osts: {nvme1n1: 6}
-        dac8:
-          abcdefgh_mdts: {nvme1n1: 7}
-          abcdefgh_osts: {nvme1n1: 7}
+          abcdefgh_mdts: {nvme1n1: 6, nvme2n1: 7}
+          abcdefgh_osts: {nvme1n1: 6, nvme2n1: 7}
         dac9:
-          abcdefgh_mdts: {nvme1n1: 8}
-          abcdefgh_osts: {nvme1n1: 8}
-        dac10:
-          abcdefgh_mdts: {nvme1n1: 9}
-          abcdefgh_osts: {nvme1n1: 9}
+          abcdefgh_mdts: {nvme1n1: 8, nvme2n1: 9}
+          abcdefgh_osts: {nvme1n1: 8, nvme2n1: 9}
         dac11:
-          abcdefgh_mdts: {nvme1n1: 10}
-          abcdefgh_osts: {nvme1n1: 10}
-        dac12:
-          abcdefgh_mdts: {nvme1n1: 11}
-          abcdefgh_osts: {nvme1n1: 11}
+          abcdefgh_mdts: {nvme1n1: 10, nvme2n1: 11}
+          abcdefgh_osts: {nvme1n1: 10, nvme2n1: 11}
         dac13:
-          abcdefgh_mdts: {nvme1n1: 12}
-          abcdefgh_osts: {nvme1n1: 12}
-        dac14:
-          abcdefgh_mdts: {nvme1n1: 13}
-          abcdefgh_osts: {nvme1n1: 13}
+          abcdefgh_mdts: {nvme1n1: 12, nvme2n1: 13}
+          abcdefgh_osts: {nvme1n1: 12, nvme2n1: 13}
         dac15:
-          abcdefgh_mdts: {nvme1n1: 14}
-          abcdefgh_osts: {nvme1n1: 14}
-        dac16:
-          abcdefgh_mdts: {nvme1n1: 15}
-          abcdefgh_osts: {nvme1n1: 15}
+          abcdefgh_mdts: {nvme1n1: 14, nvme2n1: 15}
+          abcdefgh_osts: {nvme1n1: 14, nvme2n1: 15}
         dac17:
-          abcdefgh_mdts: {nvme1n1: 16}
-          abcdefgh_osts: {nvme1n1: 16}
-        dac18:
-          abcdefgh_mdts: {nvme1n1: 17}
-          abcdefgh_osts: {nvme1n1: 17}
+          abcdefgh_mdts: {nvme1n1: 16, nvme2n1: 17}
+          abcdefgh_osts: {nvme1n1: 16, nvme2n1: 17}
         dac19:
-          abcdefgh_mdts: {nvme1n1: 18}
-          abcdefgh_osts: {nvme1n1: 18}
-        dac20:
-          abcdefgh_mdts: {nvme1n1: 19}
-          abcdefgh_osts: {nvme1n1: 19}
+          abcdefgh_mdts: {nvme1n1: 18, nvme2n1: 19}
+          abcdefgh_osts: {nvme1n1: 18, nvme2n1: 19}
         dac21:
-          abcdefgh_mdts: {nvme1n1: 20}
-          abcdefgh_osts: {nvme1n1: 20}
-        dac22:
-          abcdefgh_mdts: {nvme1n1: 21}
-          abcdefgh_osts: {nvme1n1: 21}
+          abcdefgh_mdts: {nvme1n1: 20, nvme2n1: 21}
+          abcdefgh_osts: {nvme1n1: 20, nvme2n1: 21}
         dac23:
-          abcdefgh_mdts: {nvme1n1: 22}
-          abcdefgh_osts: {nvme1n1: 22}
-        dac24:
-          abcdefgh_mdts: {nvme1n1: 23}
-          abcdefgh_osts: {nvme1n1: 23}
+          abcdefgh_mdts: {nvme1n1: 22, nvme2n1: 23}
+          abcdefgh_osts: {nvme1n1: 22, nvme2n1: 23}
         dac25:
-          abcdefgh_osts: {nvme1n1: 24}
-        dac26:
-          abcdefgh_osts: {nvme1n1: 25}
+          abcdefgh_osts: {nvme1n1: 24, nvme2n1: 25}
       vars:
         abcdefgh_client_port: "10002"
         lnet_suffix: ""
