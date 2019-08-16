7493b72d92f5bb9f6b1f67f89582461813a381f8
John Garbutt <john@johngarbutt.com>
2019-08-16 21:04:45 +0100

    Add tests for handleCreate

--- internal/pkg/v2/dacd/brick_manager_impl/session_action_handler.go
+++ internal/pkg/v2/dacd/brick_manager_impl/session_action_handler.go
@@ -1,8 +1,10 @@
 package brick_manager_impl
 
 import (
+	"context"
 	"github.com/RSE-Cambridge/data-acc/internal/pkg/v2/datamodel"
 	"github.com/RSE-Cambridge/data-acc/internal/pkg/v2/facade"
+	"github.com/RSE-Cambridge/data-acc/internal/pkg/v2/filesystem"
 	"github.com/RSE-Cambridge/data-acc/internal/pkg/v2/registry"
 	"log"
 )
@@ -12,7 +14,9 @@ func NewSessionActionHandler(actions registry.SessionActions) facade.SessionActi
 }
 
 type sessionActionHandler struct {
+	registry     registry.SessionRegistry
 	actions      registry.SessionActions
+	fsProvider   filesystem.Provider
 	skipActions  bool
 	actionCalled datamodel.SessionActionType
 }
@@ -37,21 +41,52 @@ func (s *sessionActionHandler) ProcessSessionAction(action datamodel.SessionActi
 }
 
 func (s *sessionActionHandler) handleCreate(action datamodel.SessionAction) {
-	log.Println("create")
-	s.reportComplete(action)
+	sessionName := action.Session.Name
+	sessionMutex, err := s.registry.GetSessionMutex(sessionName)
+	if err != nil {
+		log.Printf("unable to get session mutex: %s due to: %s\n", sessionName, err)
+		s.actions.CompleteSessionAction(action, err)
+		return
+	}
+	err = sessionMutex.Lock(context.TODO())
+	if err != nil {
+		log.Printf("unable to lock session mutex: %s due to: %s\n", sessionName, err)
+		s.actions.CompleteSessionAction(action, err)
+		return
+	}
+	defer func() {
+		if err := sessionMutex.Unlock(context.TODO()); err != nil {
+			log.Println("failed to drop mutex for:", sessionName)
+		}
+	}()
+	log.Printf("starting create for %+v\n", sessionName)
+
+	// Get latest session now we have the mutex
+	session, err := s.registry.GetSession(sessionName)
+
+	fsStatus, err := s.fsProvider.Create(action.Session)
+	session.FilesystemStatus = fsStatus
+	session.Status.FileSystemCreated = err == nil
+	session.Status.Error = err
+
+	session, err = s.registry.UpdateSession(session)
+	if err != nil {
+		log.Printf("Failed to update session: %+v", session)
+	} else {
+		action.Session = session
+	}
+
+	if err := s.actions.CompleteSessionAction(action, action.Session.Status.Error); err != nil {
+		log.Printf("Failed to complete action: %+v", action)
+	}
+	if action.Session.Status.Error != nil {
+		log.Println("error during create for", sessionName, err)
+	} else {
+		log.Printf("completed create for %+v\n", sessionName)
+	}
 }
 
 func (s *sessionActionHandler) handleDelete(action datamodel.SessionAction) {
 	log.Println("delete")
-	s.reportComplete(action)
-}
-
-func (s *sessionActionHandler) reportComplete(action datamodel.SessionAction) {
-	err := s.actions.CompleteSessionAction(action, nil)
-	if err != nil {
-		log.Println("Failed to complete ActionType:", err)
-		// TODO: put session into error state?
-		return
-	}
-	log.Println("Stopped processing action:", action)
+	s.actions.CompleteSessionAction(action, nil)
 }
--- internal/pkg/v2/dacd/brick_manager_impl/session_action_handler_test.go
+++ internal/pkg/v2/dacd/brick_manager_impl/session_action_handler_test.go
@@ -1,9 +1,12 @@
 package brick_manager_impl
 
 import (
+	"context"
 	"fmt"
 	"github.com/RSE-Cambridge/data-acc/internal/pkg/v2/datamodel"
+	"github.com/RSE-Cambridge/data-acc/internal/pkg/v2/mock_filesystem"
 	"github.com/RSE-Cambridge/data-acc/internal/pkg/v2/mock_registry"
+	"github.com/RSE-Cambridge/data-acc/internal/pkg/v2/mock_store"
 	"github.com/golang/mock/gomock"
 	"github.com/stretchr/testify/assert"
 	"testing"
@@ -43,13 +46,32 @@ func TestSessionActionHandler_ProcessSessionAction_Delete(t *testing.T) {
 func TestSessionActionHandler_handleCreate(t *testing.T) {
 	mockCtrl := gomock.NewController(t)
 	defer mockCtrl.Finish()
+	registry := mock_registry.NewMockSessionRegistry(mockCtrl)
 	actions := mock_registry.NewMockSessionActions(mockCtrl)
-	handler := sessionActionHandler{actions: actions}
+	fsProvider := mock_filesystem.NewMockProvider(mockCtrl)
+	handler := sessionActionHandler{
+		registry: registry, actions: actions, fsProvider: fsProvider,
+	}
 	action := datamodel.SessionAction{
 		ActionType: datamodel.SessionCreate,
+		Session:    datamodel.Session{Name: "test"},
 	}
-
-	actions.EXPECT().CompleteSessionAction(action, nil)
+	sessionMutex := mock_store.NewMockMutex(mockCtrl)
+	registry.EXPECT().GetSessionMutex(action.Session.Name).Return(sessionMutex, nil)
+	sessionMutex.EXPECT().Lock(context.TODO())
+	sessionMutex.EXPECT().Unlock(context.TODO())
+	registry.EXPECT().GetSession(action.Session.Name).Return(action.Session, nil)
+	fsProvider.EXPECT().Create(action.Session)
+	updatedSession := datamodel.Session{
+		Name:   action.Session.Name,
+		Status: datamodel.SessionStatus{FileSystemCreated: true},
+	}
+	registry.EXPECT().UpdateSession(updatedSession).Return(updatedSession, nil)
+	updatedAction := datamodel.SessionAction{
+		ActionType: datamodel.SessionCreate,
+		Session:    updatedSession,
+	}
+	actions.EXPECT().CompleteSessionAction(updatedAction, nil)
 
 	handler.handleCreate(action)
 }
