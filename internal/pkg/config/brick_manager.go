2afde66ccff428501cc68e6cb19d5aa2d40f506c
John Garbutt <john@johngarbutt.com>
2019-08-30 16:42:56 +0100

    Add backwards compatibility for config changes

--- internal/pkg/config/brick_manager.go
+++ internal/pkg/config/brick_manager.go
@@ -19,9 +19,12 @@ func GetBrickManagerConfig(env ReadEnvironemnt) BrickManagerConfig {
 	config := BrickManagerConfig{
 		datamodel.BrickHostName(getHostname(env)),
 		datamodel.PoolName(getString(env, "DAC_POOL_NAME", "default")),
-		getUint(env, "DAC_BRICK_CAPACITY_GB", 1400),             // TODO: DAC_DEVICE_CAPACITY_GB
-		getUint(env, "DAC_BRICK_COUNT", 12),                     // TODO: DEVICE_COUNT
-		getString(env, "DAC_BRICK_ADDRESS_PATTERN", "nvme%dn1"), // TODO: DEVICE_TYPE
+		getUint(env, "DAC_BRICK_CAPACITY_GB",
+			getUint(env, "DAC_DEVICE_CAPACITY_GB", 1400)),
+		getUint(env, "DAC_BRICK_COUNT",
+			getUint(env, "DEVICE_COUNT", 12)),
+		getString(env, "DAC_BRICK_ADDRESS_PATTERN",
+			getString(env, "DEVICE_TYPE", "nvme%dn1")),
 		getBool(env, "DAC_HOST_ENABLED", true),
 	}
 	log.Println("Got brick manager config:", config)
