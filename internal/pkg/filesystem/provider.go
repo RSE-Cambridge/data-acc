ca5b7379a96a0850a4766f043774598caa018b57
John Garbutt <john@johngarbutt.com>
2019-09-09 12:56:11 +0100

    Add option to mount to say if permissions set

--- internal/pkg/filesystem/provider.go
+++ internal/pkg/filesystem/provider.go
@@ -10,6 +10,6 @@ type Provider interface {
 	DataCopyIn(session datamodel.Session) error
 	DataCopyOut(session datamodel.Session) error
 
-	Mount(session datamodel.Session, attachments datamodel.AttachmentSession) error
+	Mount(session datamodel.Session, attachments datamodel.AttachmentSession, setInitialPermissions bool) error
 	Unmount(session datamodel.Session, attachments datamodel.AttachmentSession) error
 }
